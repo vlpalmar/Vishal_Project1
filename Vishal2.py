2bf005467d50e0bc4e241819e8bfa1d0d2e4aca0
vlpalmar <vishal.palmar1@gmail.com>
2026-03-07 18:12:33 +0530

    Create Vishal2.py

---
+++ Vishal2.py
@@ -0,0 +1,23 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+#from scraper import fetch_website_contents
+#from IPython.display import Markdown, display
+from openai import OpenAI
+
+#load_dotenv(override=True)
+#api_key = os.getenv('OPENAI_API_KEY')
+
+#For running remotely use this to get api key
+api_key = st.secrets['OPENAI_API_KEY']
+
+# Check the key
+
+if not api_key:
+    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
+else:
+    print("API key found and looks good so far!")
